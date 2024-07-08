648d92a3443c987f28b95f3dd62d0ee37d8c046b
“MorisLuis” <moradoluisenrique@gmail.com>
2024-07-08 16:34:40 -0600

    setup

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,11 @@
 import { Client, ClientConfig } from "pg";
 
 const clientConfig: ClientConfig = {
-    host: "192.168.1.81",
-    user: "morado",
-    password: "adiosamor2",
+    host: "grupomac.dyndns.tv",
+    user: "postgres",
+    password: "Master55",
     port: 5432,
-    database: 'mapasac'
+    database: "desarrollo"
 };
 
 let client: Client | null = null;
@@ -13,8 +13,10 @@ let client: Client | null = null;
 export const dbConnection = async () => {
     if (!client) {
         client = new Client(clientConfig);
+        console.log({client})
         try {
-            await client.connect();
+            const pool = await client.connect();
+            console.log({pool})
             console.log("Connected to the database!");
         } catch (err) {
             console.error('Error connecting to the database:', err);
